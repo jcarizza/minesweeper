5f1796ddcec2fca8fd56094a390b923179f19d93
Juan Carizza <juan.carizza@gmail.com>
2018-05-04 10:27:21 -0300

    Add react app

---
+++ mine_app/src/API.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+let API_PATH = 'http://localhost:8000';
+
+export function getToken() {
+  return localStorage.getItem('token');
+}
+
+export function getGames() {
+  
+  return axios.get(`${API_PATH}/api/game/`,
+    {
+      headers: { Authorization: `Token ${getToken()}`}
+    })
+    
+
+}
+
+export function login(username, password) {
+  return axios.post(`${API_PATH}/api/auth/`, 
+    {
+      'username': username,
+      'password': password
+    }).then((response) => {
+      localStorage.setItem('token', response.data.auth_token);
+      return response.data.auth_token;
+    });
+
+}
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
---
+++ mine_app/src/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { getGames, login } from './API';
+import './App.css';
+
+
+class LoginForm extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			username: '',
+			password: ''
+		}
+	}
+
+	doLogin(event) {
+		let foo = login(this.state.username, this.state.password).then((foo) => {
+      if (foo !== undefined) {
+        alert('logueado');
+      }
+    })
+    event.preventDefault();
+	}
+
+	handleChangePassword(event) {
+		this.setState({password: event.target.value})
+	}
+
+	handleChangeUsername(event) {
+		this.setState({username: event.target.value})
+	}
+
+  render() {
+    return (
+			<div>
+				<form className={"login-form"}>
+					<input onChange={this.handleChangeUsername.bind(this)} value={this.state.username} placeholder={"username"} type="text" name="username" />
+					<input onChange={this.handleChangePassword.bind(this)} value={this.state.password} placeholder={"password"} type="password" name="password" />
+					<button onClick={this.doLogin.bind(this)} type="submit">Login</button>
+				</form>
+			</div>	
+    )
+  }
+}
+
+export default LoginForm
