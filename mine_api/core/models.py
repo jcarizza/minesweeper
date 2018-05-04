adf1cfc568ac4dc3b2f41ddda5f7718ac355cf47
Juan Carizza <juan.carizza@gmail.com>
2018-05-03 23:39:54 -0300

    Add docstring to Game model

--- mine_api/core/models.py
+++ mine_api/core/models.py
@@ -5,6 +5,17 @@ from django.conf import settings
 
 
 class Game(models.Model):
+    """
+    Game store all information about one session
+
+    map_json: str - Must be a two dimensional array
+    map_length: int - Define the dimension of the map
+    time: int - The playing time in seconds
+    game_over: bool - If the game is finished and the player loose
+    win: bool - If the game is finished and the player won
+    user: User - The user whose belong this game
+
+    """
     map_json = models.TextField()
     map_length = models.IntegerField()
     time = models.IntegerField(default=0)
